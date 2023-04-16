10fab462ba30d29c53ef15f044108c12b5cf0265
shahidmahmood13 <shahid.mehmood@gmail.com>
2023-04-16 20:48:52 +0500

    2nd case done

--- src/Cart.js
+++ src/Cart.js
@@ -1,3 +1,26 @@
 module.exports = class Cart {
+  
+
+    constructor (){
+        this.items= [];
+        this.totalPrice=0;
+    }
+
+    getCart=()=>{
+        return this.items;
+       }
+
+
+    Additem =(item , quantity) => {
+        
+           this.items.push({...item, quantity})
+           this.totalPrice += this.items[0].price*quantity;
+ b       
+          return this.items;
+
+    }
+   
+    
+
 
 }
--- test/Cart.test.js
+++ test/Cart.test.js
@@ -1,11 +1,33 @@
-const Cart = require('../src/Cart.js');
-const expect = require('chai').expect;
+// const { expect } = require('chai');
+const { expect } = require("@jest/globals");
+
+const Cart= require('../src/Cart.js');
+const Item = require('../src/Item.js');
+// const expect = require('chai').expect;
 
 describe('Cart', () => {
     it('should initialize as empty', () => {
-        const cart = new Cart()
-
-        expect(cart.items).to.deep.equal([])
-        expect(cart.totalPrice).to.be.equal(0)
+    const cart = new Cart();
+    let assert= [];
+    let result = cart.getCart();
+    expect(result).toEqual(assert);
+    
     })
+
+    it('add item and get total price',()=>{
+        //  Setup
+        const cart = new Cart();
+        const item = new Item("hoverboard",2500, true);
+        let quantity = 2;
+        // act
+        cart.Additem(item,quantity);
+        let assert = item.price* quantity; 
+        let result= cart.totalPrice;
+        // assert  
+        expect(result).toBe(assert);
+
+    })
+
+
+
 })
